b6d4dd7f2c22dbb3d4dddc3d09f4149f34adfc54
exking <exking@users.noreply.github.com>
2026-04-08 23:35:03 -0500

    access policy

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -36,7 +36,7 @@ backend.addOutput({
         aws_region: "us-east-1",
         //@ts-expect-error amplify backend type issue https://github.com/aws-amplify/amplify-backend/issues/2569
         paths: {
-          "/": {
+          ".": {
             groupsadmin: ["get", "list", "write", "delete"],
             authenticated: ["get", "list", "write", "delete"],
           },
